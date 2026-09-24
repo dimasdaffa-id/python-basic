01b4c8be212d413dfee48612d06fcd9f88ef0539
Dimas Daffa Rasheesa <dimasdaffarasheesa@gmail.com>
2026-09-24 15:11:54 +0700

    Add data types practice

---
+++ 03_data_types.py
@@ -0,0 +1,14 @@
+name = "Dimas Daffa Rasheesa"
+age = 17
+height = 1.75
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
