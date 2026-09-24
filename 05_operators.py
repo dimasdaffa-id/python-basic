f59fdca943cc2b07614eaad728a66f19693c9647
Dimas Daffa Rasheesa <dimasdaffarasheesa@gmail.com>
2026-09-24 15:42:47 +0700

    Add operators practice

---
+++ 05_operators.py
@@ -0,0 +1,14 @@
+a = 12
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Remainder:", a % b)
+
+age = 17
+
+print("Is age 17?", age == 17)
+print("Is age greater than 15?", age > 15)
+print("Is age less than 20?", age < 20)
